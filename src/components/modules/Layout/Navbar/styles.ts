a289d38fd0960b63944b3621d37e6aa20134b880
rg-rupeee <rupeshgarhwal3920@gmail.com>
2024-05-25 17:53:49 +0530

    basic resposive navbar

--- src/components/modules/Layout/Navbar/styles.ts
+++ src/components/modules/Layout/Navbar/styles.ts
@@ -1,34 +1,44 @@
 import styled from '@emotion/styled';
 
 export const NavContainer = styled.nav`
-  top: 0;
+  font-weight: 500;
   font-size: 0.8rem;
+  margin: 1rem 0.5rem;
   position: sticky;
-  margin: 3rem 4rem;
+  top: 0;
   display: flex;
   align-items: center;
   justify-content: space-between;
   z-index: 100;
+
+  @media (min-width: 768px) {
+    margin: 3rem 4rem;
+  }
 `;
 
 export const Logo = styled.div``;
 
 export const NavList = styled.ul`
-  padding: 0.7rem;
-  display: flex;
-  border: 1px solid green;
-  border-radius: 2rem;
+  display: none;
+  backdrop-filter: blur(15px);
+  @media (min-width: 768px) {
+    padding: 0.7rem;
+    display: flex;
+    border: 1px solid var(--color-primary);
+    border-radius: 2rem;
+  }
 `;
 
 export const NavItem = styled.ul`
-  margin: 0 0.5rem;
-  padding: 0.3rem 0.7rem;
-  display: flex;
-  border-radius: 2rem;
-  border: 1px solid green;
-
-  
   &:hover {
-    background-color: green;
+    color: var(--color-black);
+    background-color: var(--color-primary);
+  }
+
+  @media (min-width: 768px) {
+    margin: 0 0.5rem;
+    padding: 0.3rem 0.7rem;
+    display: flex;
+    border-radius: 2rem;
   }
 `;
